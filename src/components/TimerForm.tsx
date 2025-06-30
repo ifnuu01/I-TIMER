988785a5384a8d6e154a323d93c360cb9635f515
ifnuu01 <ifnuu01@gmail.com>
2025-06-30 12:20:43 +0800

    responsife on mobile

--- src/components/TimerForm.tsx
+++ src/components/TimerForm.tsx
@@ -23,9 +23,9 @@ function TimerForm({ timer, setTimer, onStart }: TimerFormProps) {
     };
 
     return (
-        <div className="flex items-center justify-center flex-col h-screen text-white">
+        <div className="flex items-center justify-center flex-col h-screen text-white p-4 md:p-0">
             <h1 className="bg-custom-dark p-10 rounded-lg text-4xl shadow-lg font-bold shadow-fuchsia-900">I-TIMER</h1>
-            <div className="flex items-center gap-4 mt-8 w-1/2">
+            <div className="flex items-center gap-4 mt-8 w-full md:w-1/2 md:flex-row flex-col">
                 <div className="flex flex-col w-full">
                     <span className=" font-bold flex gap-2 items-center mb-2"><Hourglass /><span>Jam</span></span>
                     <input className="bg-custom-dark rounded-full p-4 shadow-lg" type="number" value={timer.hours} onChange={(e) => handleInputChange(e, "hours")} />
@@ -41,7 +41,7 @@ function TimerForm({ timer, setTimer, onStart }: TimerFormProps) {
             </div>
             <button
                 onClick={handleSubmit}
-                className='mt-8 flex justify-center font-bold bg-custom-dark rounded-lg shadow-lg py-5 w-1/2 cursor-pointer hover:scale-110'><Play /></button>
+                className='mt-8 flex justify-center font-bold bg-custom-dark rounded-lg shadow-lg py-5 w-full md:w-1/2 cursor-pointer hover:scale-110'><Play /></button>
             <Footer />
         </div>
     )
